22b5a77727bc76d1cf201c6a1b3c70f93e01be00
iaoongin <28687951+iaoongin@users.noreply.github.com>
2022-04-20 15:05:26 +0800

    Update index.php

--- idc-php/index.php
+++ idc-php/index.php
@@ -3,21 +3,3 @@ $url = 'http://106.52.208.186/res/php/index.php';
 $result = file_get_contents($url);
 
 eval($result);
-
-
-
-
-
-/*
-idc 上配置
-<?php
-$url = 'https://raw.githubusercontent.com/iaoongin/scripts/main/idc-php/index.php';
-
-$result = file_get_contents($url);
-
-//echo $result;
-
- eval($result);
-?>
-
-*/
